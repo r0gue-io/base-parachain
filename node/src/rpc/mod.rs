8a4ba61248bc65d3db630f42b51ee2c0610ee62c
AlexD10S <alexfraga10@gmail.com>
2024-05-02 19:42:43 +0200

    feat: add frontier

---
+++ node/src/rpc/mod.rs
@@ -0,0 +1,75 @@
+//! A collection of node-specific RPC methods.
+//! Substrate provides the `sc-rpc` crate, which defines the core RPC layer
+//! used by Substrate nodes. This file extends those RPC definitions with
+//! capabilities that are specific to this project's runtime configuration.
+
+#![warn(missing_docs)]
+
+use std::sync::Arc;
+
+use frontier_parachain_runtime::{opaque::Block, AccountId, Balance, Nonce};
+
+pub use sc_rpc::DenyUnsafe;
+use sc_transaction_pool::ChainApi;
+use sc_transaction_pool_api::TransactionPool;
+use sp_api::ProvideRuntimeApi;
+use sp_block_builder::BlockBuilder;
+use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
+
+/// A type representing all RPC extensions.
+pub type RpcExtension = jsonrpsee::RpcModule<()>;
+
+/// Full client dependencies
+pub struct FullDeps<C, P, A: ChainApi, CT> {
+    /// The client instance to use.
+    pub client: Arc<C>,
+    /// Transaction pool instance.
+    pub pool: Arc<P>,
+    /// Whether to deny unsafe calls
+    pub deny_unsafe: DenyUnsafe,
+    // /// Ethereum-compatibility specific dependencies.
+    pub eth: EthDeps<C, P, A, CT, Block>,
+}
+
+/// Instantiate all RPC extensions.
+pub fn create_full<C, P, BE, A, CT>(
+    deps: FullDeps<C, P, A, CT>,
+    subscription_task_executor: SubscriptionTaskExecutor,
+) -> Result<RpcExtension, Box<dyn std::error::Error + Send + Sync>>
+where
+    BE: Backend<Block> + 'static,
+    BE::State: StateBackend<BlakeTwo256>,
+    C: ProvideRuntimeApi<Block>
+        + StorageProvider<Block, BE>
+        + BlockchainEvents<Block>
+        + HeaderBackend<Block>
+        + HeaderMetadata<Block, Error = BlockChainError>
+        + Send
+        + Sync
+        + 'static,
+    C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+    C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
+    C::Api: BlockBuilder<Block>,
+    C::Api: fp_rpc::ConvertTransactionRuntimeApi<Block>,
+    C::Api: fp_rpc::EthereumRuntimeRPCApi<Block>,
+    P: TransactionPool<Block = Block> + Sync + Send + 'static,
+    A: ChainApi<Block = Block> + 'static,
+    CT: fp_rpc::ConvertTransaction<<Block as BlockT>::Extrinsic> + Send + Sync + 'static,
+{
+    use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
+    use substrate_frame_rpc_system::{System, SystemApiServer};
+
+    let mut module = RpcExtension::new(());
+    let FullDeps {
+        client,
+        pool,
+        deny_unsafe,
+        eth,
+    } = deps;
+
+    module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
+    module.merge(TransactionPayment::new(client).into_rpc())?;
+    // Ethereum compatibility RPCs
+    let module = create_eth::<_, _, _, _, _, _>(module, eth, subscription_task_executor)?;
+    Ok(module)
+}
