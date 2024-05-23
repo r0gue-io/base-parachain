0b2d5f92a30140452d995da886cf93ccc4e5027a
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2024-05-23 20:42:42 +0200

    chore(base): polkadot-v1.11.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -288,23 +288,10 @@ construct_runtime!(
     }
 );
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benches {
-    frame_benchmarking::define_benchmarks!(
-        // Only benchmark the following pallets
-        [frame_system, SystemBench::<Runtime>]
-        [cumulus_pallet_parachain_system, ParachainSystem]
-        [pallet_timestamp, Timestamp]
-        [pallet_balances, Balances]
-        [pallet_sudo, Sudo]
-        [pallet_collator_selection, CollatorSelection]
-        [pallet_session, SessionBench::<Runtime>]
-        [cumulus_pallet_xcmp_queue, XcmpQueue]
-        [pallet_message_queue, MessageQueue]
-    );
-}
-
 cumulus_pallet_parachain_system::register_validate_block! {
     Runtime = Runtime,
     BlockExecutor = cumulus_pallet_aura_ext::BlockExecutor::<Runtime, Executive>,
 }
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarks;
