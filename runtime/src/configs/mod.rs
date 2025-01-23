82741b4e597438d2f7a4208850e94916be25b462
gatsey <42411328+f-gate@users.noreply.github.com>
2025-01-23 09:00:05 +0100

    chore: upgrade to stable2412 (#53)
    * 2412 upgrade
    
    * fmt
    
    * clippy
    
    * TransactionExtension and node refactor
    
    * fmt
    
    * sp_keyring
    
    * stray-key-gen
    
    * todo and runtime-benchmark warning
    
    ---------
    
    Co-authored-by: f-gate <felix@r0gue.io>

--- runtime/src/configs/mod.rs
+++ runtime/src/configs/mod.rs
@@ -159,6 +159,7 @@ impl pallet_balances::Config for Runtime {
     type RuntimeFreezeReason = RuntimeFreezeReason;
     type FreezeIdentifier = RuntimeFreezeReason;
     type MaxFreezes = VariantCountOf<RuntimeFreezeReason>;
+    type DoneSlashHandler = ();
 }
 
 parameter_types! {
@@ -173,6 +174,7 @@ impl pallet_transaction_payment::Config for Runtime {
     type LengthToFee = ConstantMultiplier<Balance, TransactionByteFee>;
     type FeeMultiplierUpdate = SlowAdjustingFeeUpdate<Self>;
     type OperationalFeeMultiplier = ConstU8<5>;
+    type WeightInfo = ();
 }
 
 impl pallet_sudo::Config for Runtime {
@@ -199,6 +201,7 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
     type ReservedXcmpWeight = ReservedXcmpWeight;
     type CheckAssociatedRelayNumber = RelayNumberMonotonicallyIncreases;
     type ConsensusHook = ConsensusHook;
+    type SelectCore = cumulus_pallet_parachain_system::DefaultCoreSelector<Runtime>;
 }
 
 impl parachain_info::Config for Runtime {}
